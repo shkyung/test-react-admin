f2547edc8dff183b3a218bab34d19f4b0909dad7
sunny kyung <shkyung@lunit.io>
2020-07-16 17:11:35 +0900

    Handling Relationships

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import { Admin, Resource } from 'react-admin'
 import { UserList } from './users'
+import { PostList } from './posts'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} />
       <Resource name="users" list={UserList} />
     </Admin>
   )
