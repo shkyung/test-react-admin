7bd4a61896554354063877b6dc4dd72df144ad88
sunny kyung <shkyung@lunit.io>
2020-07-16 15:10:12 +0900

    Selecting Columns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {Admin, Resource, ListGuesser} from 'react-admin'
+import React from 'react'
+import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import { UserList } from './users'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name='users' list={ListGuesser}/>
+      <Resource name="users" list={UserList} />
     </Admin>
   )
 }
 
-export default App;
+export default App
