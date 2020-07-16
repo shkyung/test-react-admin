ab43f71dc5f28eb95f0b72a0441bd423cb784e5e
sunny kyung <shkyung@lunit.io>
2020-07-16 14:14:36 +0900

    Making Contact With The API Using a Data Provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Admin, Resource} from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Admin dataProvider={dataProvider}/>
+  )
 }
 
 export default App;
