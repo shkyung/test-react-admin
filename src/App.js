66ec534f4b8dbb863abf27b76e7a9485feb998fe
sunny kyung <shkyung@lunit.io>
2020-07-18 03:32:32 +0900

    Customizing the Menu Icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import jsonServerProvider from 'ra-data-json-server'
 import { Admin, Resource, EditGuesser } from 'react-admin'
+import PostIcon from '@material-ui/icons/Book'
+import UserIcon from '@material-ui/icons/Group'
 import { UserList } from './users'
 import { PostList, PostEdit, PostCreate } from './posts'
 
@@ -14,8 +16,9 @@ function App() {
         list={PostList}
         edit={PostEdit}
         create={PostCreate}
+        icon={PostIcon}
       />
-      <Resource name="users" list={UserList} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
   )
 }
