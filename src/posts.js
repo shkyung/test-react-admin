67ca11da42ee3729aa3c2801cfb76e1e023805cc
sunny kyung <shkyung@lunit.io>
2020-07-18 03:11:09 +0900

    Adding Search And Filters To The List

--- src/posts.js
+++ src/posts.js
@@ -10,11 +10,21 @@ import {
   ReferenceInput,
   SelectInput,
   TextInput,
-  Create
+  Create,
+  Filter
 } from 'react-admin'
 
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+)
+
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
