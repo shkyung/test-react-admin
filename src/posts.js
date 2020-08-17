71f842360c0cfadbe892799bd9f4b54cd9d46965
sunny kyung <shkyung@lunit.io>
2020-08-17 18:03:29 +0900

    bulkAction and plain action in PostList

--- src/posts.js
+++ src/posts.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { cloneElement, useMemo, Fragment } from 'react'
+import PropTypes from 'prop-types'
 import {
   List,
   Datagrid,
@@ -11,21 +12,96 @@ import {
   SelectInput,
   TextInput,
   Create,
-  Filter
+  Filter,
+  useListContext,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  Button,
+  sanitizeListRestProps,
+  BulkDeleteButton
 } from 'react-admin'
+
+import IconEvent from '@material-ui/icons/Event'
+import ResetViewsButton from './ResetViewsButton'
+
+const ListActions = (props) => {
+  const { className, exporter, filters, maxResults, ...rest } = props
+  const {
+    currentSort,
+    resource,
+    displayedFilters,
+    filterValues,
+    hasCreate,
+    basePath,
+    selectedIds,
+    showFilter,
+    total
+  } = useListContext()
+  return (
+    <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+      {filters &&
+        cloneElement(filters, {
+          resource,
+          showFilter,
+          displayedFilters,
+          filterValues,
+          context: 'button'
+        })}
+      <CreateButton basePath={basePath} />
+      {/* 필요없는 export버튼을 비활성화 하는 방법*/}
+      {/*<ExportButton*/}
+      {/*  disabled={total === 0}*/}
+      {/*  resource={resource}*/}
+      {/*  sort={currentSort}*/}
+      {/*  filterValues={filterValues}*/}
+      {/*  maxResults={maxResults}*/}
+      {/*/>*/}
+      {/* Add your custom actions */}
+      <Button
+        onClick={() => {
+          alert('Your custom action')
+        }}
+        label="Show calendar"
+      >
+        <IconEvent />
+      </Button>
+    </TopToolbar>
+  )
+}
 
 const PostFilter = (props) => (
   <Filter {...props}>
     <TextInput label="Search" source="q" alwaysOn />
-    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+    <ReferenceInput
+      label="User"
+      source="userId"
+      reference="users"
+      allowEmpty
+      alwaysOn
+    >
       <SelectInput optionText="name" />
     </ReferenceInput>
   </Filter>
 )
 
+const PostBulkActionButtons = (props) => (
+  <Fragment>
+    <ResetViewsButton label="Reset Views" {...props} />
+    {/* default bulk delete action */}
+    <BulkDeleteButton {...props} />
+  </Fragment>
+)
+
 export const PostList = (props) => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
+  <List
+    title="List of posts"
+    bulkActionButtons={<PostBulkActionButtons />}
+    actions={<ListActions />}
+    filters={<PostFilter />}
+    {...props}
+  >
+    <Datagrid rowClick="edit">
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
         <TextField source="name" />
